0d9bf2ac808d61da9602d0000a8bc9e3fb7929e8
aftertype420 <williamgrace86@gmail.com>
2026-06-05 00:30:28 -0700

    new file:   UnityProject/Assets/Scripts/Editor/DroneSceneBootstrapperEditor.cs

---
+++ UnityProject/Assets/Scripts/Editor/DroneSceneBootstrapperEditor.cs
@@ -0,0 +1,32 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[CustomEditor(typeof(DroneSceneBootstrapper))]
+public class DroneSceneBootstrapperEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GUILayout.Space(12);
+
+        GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+        buttonStyle.fontStyle = FontStyle.Bold;
+        buttonStyle.fixedHeight = 36;
+
+        if (GUILayout.Button("Build Spherical Drone Scene", buttonStyle))
+        {
+            DroneSceneBootstrapper builder = (DroneSceneBootstrapper)target;
+            builder.BuildScene();
+
+            if (!Application.isPlaying)
+            {
+                EditorUtility.SetDirty(builder);
+                EditorSceneManager.MarkSceneDirty(builder.gameObject.scene);
+            }
+        }
+    }
+}
+#endif
